b99e10c227f510ef576553e8948991209f63af9f
Adam Young <me@adam-young.co.uk>
2021-05-18 23:53:19 +0100

    Fixed code smell with CrimesEndpoint

--- Sources/UKPoliceData/Services/Crimes/CrimesEndpoint.swift
+++ Sources/UKPoliceData/Services/Crimes/CrimesEndpoint.swift
@@ -3,6 +3,9 @@ import Foundation
 enum CrimesEndpoint {
 
     static let basePath = URL(string: "/")!
+    static let crimesAtLocationBasePath = basePath.appendingPathComponent("crimes-at-location")
+    static let outcomesAtLocationBasePath = basePath.appendingPathComponent("outcomes-at-location")
+    static let crimeCategoriesBasePath = basePath.appendingPathComponent("crime-categories")
 
     case streetLevelCrimesAtSpecificPoint(coordinate: Coordinate, date: Date? = nil)
     case streetLevelCrimesInArea(coordinates: [Coordinate], date: Date? = nil)
@@ -18,40 +21,34 @@ extension CrimesEndpoint: Endpoint {
     var url: URL {
         switch self {
         case .streetLevelCrimesAtSpecificPoint(let coordinate, let date):
-            return Self.basePath
-                .appendingPathComponent("crimes-at-location")
+            return Self.crimesAtLocationBasePath
                 .appendingQueryItem(name: "lat", value: coordinate.latitude)
                 .appendingQueryItem(name: "lng", value: coordinate.longitude)
                 .appendingQueryItem(name: "date", value: date)
 
         case .streetLevelCrimesInArea(let coordinates, let date):
-            return Self.basePath
-                .appendingPathComponent("crimes-at-location")
+            return Self.crimesAtLocationBasePath
                 .appendingQueryItem(name: "poly", value: coordinates)
                 .appendingQueryItem(name: "date", value: date)
 
         case .streetLevelOutcomesForStreet(let streetID, let date):
-            return Self.basePath
-                .appendingPathComponent("outcomes-at-location")
+            return Self.outcomesAtLocationBasePath
                 .appendingQueryItem(name: "location_id", value: streetID)
                 .appendingQueryItem(name: "date", value: date)
 
         case .streetLevelOutcomesAtSpecificPoint(let coordinate, let date):
-            return Self.basePath
-                .appendingPathComponent("outcomes-at-location")
+            return Self.outcomesAtLocationBasePath
                 .appendingQueryItem(name: "lat", value: coordinate.latitude)
                 .appendingQueryItem(name: "lng", value: coordinate.longitude)
                 .appendingQueryItem(name: "date", value: date)
 
         case .streetLevelOutcomesInArea(let coordinates, let date):
-            return Self.basePath
-                .appendingPathComponent("outcomes-at-location")
+            return Self.outcomesAtLocationBasePath
                 .appendingQueryItem(name: "poly", value: coordinates)
                 .appendingQueryItem(name: "date", value: date)
 
         case .categories(let date):
-            return Self.basePath
-                .appendingPathComponent("crime-categories")
+            return Self.crimeCategoriesBasePath
                 .appendingQueryItem(name: "date", value: date, formatter: .yearMonth)
         }
     }
