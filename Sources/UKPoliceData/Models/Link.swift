f8c9de1479e291a72022ae2d936789417cf77825
Adam Young <me@adam-young.co.uk>
2021-05-24 15:59:25 +0100

    BUG: Fetch Neighbourhood details fails when link url is empty string

--- Sources/UKPoliceData/Models/Link.swift
+++ Sources/UKPoliceData/Models/Link.swift
@@ -8,7 +8,11 @@ public struct Link: Decodable, Equatable {
     /// Description of the link.
     public let description: String?
     /// URL.
-    public let url: URL
+    public var url: URL? {
+        URL(string: urlString)
+    }
+
+    private let urlString: String
 
     /// Creates a a new `Link`.
     ///
@@ -16,10 +20,20 @@ public struct Link: Decodable, Equatable {
     ///     - title: Title of the link.
     ///     - description: Description of the link.
     ///     - url: URL.
-    public init(title: String, description: String? = nil, url: URL) {
+    public init(title: String, description: String? = nil, url: URL? = nil) {
         self.title = title
         self.description = description
-        self.url = url
+        self.urlString = url?.absoluteString ?? ""
+    }
+
+}
+
+extension Link {
+
+    private enum CodingKeys: String, CodingKey {
+        case title
+        case description
+        case urlString = "url"
     }
 
 }
--- Tests/UKPoliceDataTests/Mocks/Models/Link+Mocks.swift
+++ Tests/UKPoliceDataTests/Mocks/Models/Link+Mocks.swift
@@ -7,14 +7,22 @@ extension Link {
         Link(
             title: "Leicester City Council",
             description: "Leicester City Council description",
-            url: URL(string: "http://www.leicester.gov.uk/")!
+            url: URL(string: "http://www.leicester.gov.uk/")
         )
     }
 
     static var mockNoDescription: Link {
         Link(
             title: "Leicester City Council",
-            url: URL(string: "http://www.leicester.gov.uk/")!
+            url: URL(string: "http://www.leicester.gov.uk/")
+        )
+    }
+
+    static var mockNilURL: Link {
+        Link(
+            title: "YourCommunityAlerts.co.uk",
+            description: "Your Community Alerts",
+            url: nil
         )
     }
 
