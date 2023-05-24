d4fa04440ea12b920a59eafddec7b3207e8900e4
Adam Young <me@adam-young.co.uk>
2023-05-24 22:12:36 +0100

    FEATURE: Add Combine publisher alternatives to coordinate based CrimeService functions (#90)

--- Tests/PoliceDataKitTests/Crimes/CrimeServiceTests.swift
+++ Tests/PoliceDataKitTests/Crimes/CrimeServiceTests.swift
@@ -1,3 +1,4 @@
+import Combine
 import MapKit
 @testable import PoliceDataKit
 import XCTest
@@ -8,17 +9,20 @@ final class CrimeServiceTests: XCTestCase {
     var apiClient: MockAPIClient!
     var cache: CrimeMockCache!
     var availableDataRegion: MKCoordinateRegion!
+    var cancellables: Set<AnyCancellable>!
 
     override func setUp() {
         super.setUp()
         apiClient = MockAPIClient()
         cache = CrimeMockCache()
         availableDataRegion = .test
+        cancellables = Set<AnyCancellable>()
         service = CrimeService(apiClient: apiClient, cache: cache, availableDataRegion: availableDataRegion)
     }
 
     override func tearDown() {
         service = nil
+        cancellables = nil
         availableDataRegion = nil
         cache = nil
         apiClient = nil
@@ -32,6 +36,32 @@ final class CrimeServiceTests: XCTestCase {
         apiClient.add(response: .success(Crime.mocks))
 
         let result = try await service.streetLevelCrimes(at: coordinate, date: date)
+
+        XCTAssertEqual(result, expectedResult)
+        XCTAssertEqual(apiClient.requestedURLs.count, 1)
+        XCTAssertEqual(
+            apiClient.requestedURLs.last,
+            CrimesEndpoint.streetLevelCrimesAtSpecificPoint(coordinate: coordinate, date: date).path
+        )
+    }
+
+    func testStreetLevelCrimesPublisherAtCoordinateReturnsCrimes() {
+        let coordinate = CLLocationCoordinate2D.mock
+        let date = Date()
+        let expectedResult = Crime.mocks
+        apiClient.add(response: .success(Crime.mocks))
+
+        let expectation = self.expectation(description: "StreetLevelCrimesPublisher")
+        var result: [Crime]?
+        service.streetLevelCrimesPublisher(at: coordinate, date: date)
+            .sink { _ in
+                expectation.fulfill()
+            } receiveValue: { crimes in
+                result = crimes
+            }
+            .store(in: &cancellables)
+
+        wait(for: [expectation])
 
         XCTAssertEqual(result, expectedResult)
         XCTAssertEqual(apiClient.requestedURLs.count, 1)
@@ -64,6 +94,32 @@ final class CrimeServiceTests: XCTestCase {
         apiClient.add(response: .success(Crime.mocks))
 
         let result = try await service.streetLevelCrimes(in: coordinates, date: date)
+
+        XCTAssertEqual(result, expectedResult)
+        XCTAssertEqual(apiClient.requestedURLs.count, 1)
+        XCTAssertEqual(
+            apiClient.requestedURLs.last,
+            CrimesEndpoint.streetLevelCrimesInArea(coordinates: CLLocationCoordinate2D.mocks, date: date).path
+        )
+    }
+
+    func testStreetLevelCrimesPublisherInAreaReturnsCrimes() {
+        let coordinates = CLLocationCoordinate2D.mocks
+        let date = Date()
+        let expectedResult = Crime.mocks
+        apiClient.add(response: .success(Crime.mocks))
+
+        let expectation = self.expectation(description: "StreetLevelCrimesPublisher")
+        var result: [Crime]?
+        service.streetLevelCrimesPublisher(in: coordinates, date: date)
+            .sink { _ in
+                expectation.fulfill()
+            } receiveValue: { crimes in
+                result = crimes
+            }
+            .store(in: &cancellables)
+
+        wait(for: [expectation])
 
         XCTAssertEqual(result, expectedResult)
         XCTAssertEqual(apiClient.requestedURLs.count, 1)
@@ -119,6 +175,32 @@ final class CrimeServiceTests: XCTestCase {
         apiClient.add(response: .success(Crime.mocks))
 
         let result = try await service.crimes(at: coordinate, date: date)
+
+        XCTAssertEqual(result, expectedResult)
+        XCTAssertEqual(apiClient.requestedURLs.count, 1)
+        XCTAssertEqual(
+            apiClient.requestedURLs.last,
+            CrimesEndpoint.crimesAtLocationAtSpecificPoint(coordinate: coordinate, date: date).path
+        )
+    }
+
+    func testCrimesPublisherAtCoordinateReturnsCrimes() {
+        let coordinate = CLLocationCoordinate2D.mock
+        let date = Date()
+        let expectedResult = Crime.mocks
+        apiClient.add(response: .success(Crime.mocks))
+
+        let expectation = self.expectation(description: "StreetLevelCrimesPublisher")
+        var result: [Crime]?
+        service.crimesPublisher(at: coordinate, date: date)
+            .sink { _ in
+                expectation.fulfill()
+            } receiveValue: { crimes in
+                result = crimes
+            }
+            .store(in: &cancellables)
+
+        wait(for: [expectation])
 
         XCTAssertEqual(result, expectedResult)
         XCTAssertEqual(apiClient.requestedURLs.count, 1)
